9906b2e20f04fd19e2d681dafbf1c91c45a26e69
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-06-08 21:32:21 +0530

    Dilated Block

--- model.py
+++ model.py
@@ -2,3 +2,17 @@ import torch
 import torch.nn as nn 
 import torchvision.transforms.functional as TF 
 
+class DilatedConv(nn.Module):
+
+    def __init__(self, in_channel, out_channel):
+        super(DilatedConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, kernel_size= 9, stride= 1, padding= 0, dilation= 2),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+    

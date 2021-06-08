d83f8693a5eeec92fa815f972abf566ed80e15fb
ayushmankumar7 <ayushmankumar7@gmail.com>
2021-06-08 21:39:16 +0530

    Up and Down Dilated Block

--- model.py
+++ model.py
@@ -2,12 +2,12 @@ import torch
 import torch.nn as nn 
 import torchvision.transforms.functional as TF 
 
-class DilatedConv(nn.Module):
+class UpDilatedConv(nn.Module):
 
     def __init__(self, in_channel, out_channel):
-        super(DilatedConv, self).__init__()
+        super(UpDilatedConv, self).__init__()
         self.conv = nn.Sequential(
-            nn.Conv2d(in_channel, out_channel, kernel_size= 9, stride= 1, padding= 0, dilation= 2),
+            nn.Conv2d(in_channel, out_channel, kernel_size= 3, stride= 2, padding= 0, dilation= 2),
             nn.ReLU()
         )
 
@@ -15,4 +15,15 @@ class DilatedConv(nn.Module):
         return self.conv(x)
 
 
-    
+
+class DownDilatedConv(nn.Module):
+
+    def __init__(self, in_channel, out_channel):
+        super(DownDilatedConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, kernel_size= 4, stride= 4, padding= 0, dilation= 2),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.conv(x)

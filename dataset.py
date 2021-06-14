851008319eb8f6a351eccd4698b29a4335c1cf2a
Srinjoy Bhuiya <srinjoybhuiya@gmail.com>
2021-06-14 18:56:08 +0530

    Fixed issue in the transformation application

--- dataset.py
+++ dataset.py
@@ -20,7 +20,7 @@ class CityScapeDataset(Dataset):
         mask = np.array(Image.open(mask_path).convert("L"), dtype= np.float32)
         mask[mask == 255.0] = 1.0 
 
-        if self.transform is None:
+        if self.transform is  not None:
             augmentations = self.transform(image = image, mask = mask)
             image = augmentations["image"]
             mask = augmentations["mask"]
